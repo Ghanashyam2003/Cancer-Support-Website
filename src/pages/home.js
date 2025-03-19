071deff3fac4862ce28c8f6dfb4a8a59532b8f5c
ghanashyam2003 <gamerxgaurav44@gmail.com>
2025-03-19 22:33:37 +0530

    Makeing Welcome page

---
+++ src/pages/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../assets/styles/style.css"; // Import CSS
+
+//main page of the website
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to Cancer Support Website</h1>
+      <p>
+        We provide resources and support for cancer patients and their families.
+      </p>
+      <button>Learn More</button>
+    </div>
+  );
+}
+
+export default Home;
